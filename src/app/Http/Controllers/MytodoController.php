e15ad9b4d25f972c057444810b3bb0af81f06e9f
adachi <adachi@launchcraft.jp>
2022-01-05 20:50:05 +0900

    コメントを追加しました

--- src/app/Http/Controllers/MytodoController.php
+++ src/app/Http/Controllers/MytodoController.php
@@ -1,11 +1,13 @@
 <?php
- 
+
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
- 
+
 class MytodoController extends Controller
 {
+    // public function 関数名(){
     public function index(){
+        // return view('返したいbladeファイル');
         return view('todos.index');
     }
 }
