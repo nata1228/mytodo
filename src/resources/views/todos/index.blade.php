bf4703cb612c49cf8e8d3093dfdbf432cf51adbf
Takumi <>
2022-01-11 20:02:37 +0900

    一覧画面のコーディング途中

--- src/resources/views/todos/index.blade.php
+++ src/resources/views/todos/index.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+
     <title>Todoリスト</title>
 </head>
 <body>
-    
+        <input type="text">
+        <button>追加</button>
+<div class="a">
+    <div class="b">
+        <p>タスク内容</p>
+        <p>期日</p>
+        <button type="submit">編集</button>
+        <button type="submit">削除</button>
+    </div>
+</div>
 </body>
 </html>
